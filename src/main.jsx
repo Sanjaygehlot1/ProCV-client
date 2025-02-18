1e9451ac3c93bb2c7e30ad560e9b290529756dab
Sanjaygehlot1 <sanjaygehlot1695@gmail.com>
2025-02-18 22:26:39 +0530

    feat: implement LivePreview and AddProjects components; update import paths for consistency

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
 import { createRoot } from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'; // Import PersistGate
-import { store, persistor } from './Store/store'; // Ensure correct store import
+import { PersistGate } from 'redux-persist/integration/react'; 
+import { store, persistor } from './Store/store'; 
 
 import App from './App.jsx';
 import Templates from './Pages/Templates.jsx';
 import GetStarted from './Pages/GetStarted';
 import FinalPage from './Pages/FinalPage';
 import ResumeBuilder from './Pages/ResumeBuilderPage.jsx';
-import PersonalDetails from './components/React Components/PersonalDetails.jsx';
-import Education from './components/React Components/Education.jsx';
+import PersonalDetails from './components/React_Components/PersonalDetails.jsx';
+import Education from './components/React_Components/Education.jsx';
 
 import './index.css';
-import Experience from './components/React Components/Experience';
-import Skills from './components/React Components/Skills';
-import About from './components/React Components/About';
-import AddProjects from './components/React Components/Projects';
+import Experience from './components/React_Components/Experience';
+import Skills from './components/React_Components/Skills';
+import About from './components/React_Components/About';
+import AddProjects from './components/React_Components/Projects';
 
 const router = createBrowserRouter([
   {
